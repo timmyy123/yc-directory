8ae76db8181782beb41b5c3117b5c67a1a3d178f
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-01-24 23:42:03 +1100

    Add initial components, authentication, and layout structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  return (
+    <>
+    <section className="pink_container">
+
+      <h1 className="heading">
+        Pitch Your Startup, <br /> Connect With Enterpreneurs
+      </h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+      </p>
+      <SearchForm/>
+    </section>
+    </>
+  );
+}
