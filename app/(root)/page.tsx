d6a725fa88c5ccd38a5871b120e69fb22555ef38
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-01-25 19:17:11 +1100

    feat: startupcard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,44 @@
-import SearchForm from "../components/SearchForm";
+// import { SearchParams } from "next/dist/server/request/search-params";
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {_id: 1, name: 'Timmy'},
+      _id: 1,
+      description: 'This is a description',
+      image: './logo.png',
+      category: 'Robots',
+      title: 'We Robots'
+    }
+  ]
   return (
     <>
-    <section className="pink_container">
-
-      <h1 className="heading">
-        Pitch Your Startup, <br /> Connect With Enterpreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
-      </p>
-      <SearchForm/>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {posts?.length > 0 ? posts.map((post: StartupCardType, index: number) => <StartupCard
+          key={post?._id} post={post}/>) : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid"></ul>
+      </section>
     </>
   );
 }
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
+import {Search} from 'lucide-react'
 
-const SearchForm = () => {
-  const query = 'test'
+const SearchForm = ({query}: {query?: string}) => {
 
-  const reset = () => {
-    const form = document.querySelector('.search-form') as HTMLFormElement
-    if(form) form.reset()
-  }
+  
   return (
     <Form action='/' scroll={false} className='search-form'>
       <input name='query' defaultValue={query} className='search-input' placeholder='Search Startups' />
       <div className='flex gap-2'>
-        {query && (
-          <button></button>
-        )}
+        {query && <SearchFormReset/>}
+        <button type='submit' className='search-btn text-white'>
+          <Search className='size-5'/>
+        </button>
       </div>
     </Form>
   )
