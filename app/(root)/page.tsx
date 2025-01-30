03bf5e641ba105270bba762a948797921abf7945
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-01-30 18:02:53 +1100

    fix: replace sanityFetch with client.fetch for improved data retrieval

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { client } from "@/sanity/lib/client";
 
 export default async function Home({
   searchParams,
@@ -11,7 +12,8 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const posts = await client.fetch(STARTUPS_QUERY, params);
+  // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
   return (
     <>
       <section className="pink_container">
@@ -39,7 +41,7 @@ export default async function Home({
           )}
         </ul>
       </section>
-      <SanityLive />
+      {/* <SanityLive /> */}
     </>
   );
 }
