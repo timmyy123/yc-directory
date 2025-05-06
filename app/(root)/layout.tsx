d0f5096506c9e9b6d08f6c8006cd88c9cbd452d5
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-05-06 17:12:43 +1000

    fix: reorder Analytics component placement in Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,8 +6,8 @@ export default function Layout({children}: Readonly<{children: React.ReactNode}>
   return(
     <main className="font-work-sans">
       <Navbar/>
-      <Analytics />
       {children}
+      <Analytics />
     </main>
   )
 }
