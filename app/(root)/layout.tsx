1a1fecec1c1ae1aaca83b2c5d4654ad74876fb49
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-05-06 18:09:45 +1000

    fix: remove Analytics component from Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({children}: Readonly<{children: React.ReactNode}>){
   return(
     <main className="font-work-sans">
       <Navbar/>
       {children}
-      <Analytics/>
     </main>
   )
 }
